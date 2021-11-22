7b56f51ef5213d3cf6b38970f6ff1dff7414386b
PauloDev13 <devpgm2020@gmail.com>
2021-11-22 10:14:20 -0300

    Implementing the Save Category method

--- src/main/java/com/gvendas/gestaogvendas/controllers/CategoriaController.java
+++ src/main/java/com/gvendas/gestaogvendas/controllers/CategoriaController.java
@@ -2,11 +2,9 @@ package com.gvendas.gestaogvendas.controllers;
 
 import com.gvendas.gestaogvendas.entities.Categoria;
 import com.gvendas.gestaogvendas.services.CategoriaService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -29,5 +27,10 @@ public class CategoriaController {
   public ResponseEntity<Optional<Categoria>> findById(@PathVariable Long codigo) {
     Optional<Categoria> categoria = categoriaService.findById(codigo);
     return categoria.isPresent() ? ResponseEntity.ok(categoria) : ResponseEntity.notFound().build();
+  }
+
+  @PostMapping
+  public ResponseEntity<Categoria> save(@RequestBody() Categoria categoria) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(categoriaService.save(categoria));
   }
 }
